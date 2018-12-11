26bbc8ee02006634178bb9c03749be3f750b8e64
marabesi <matheus.marabesi@gmail.com>
2018-12-11 22:13:24 +0100

    symfony delivery app base

---
+++ delivery/README.md
@@ -0,0 +1,4 @@
+symfony-fun
+===========
+
+A Symfony project created on December 4, 2018, 5:02 pm.
---
+++ delivery/src/AppBundle/Entity/Task.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\GeneratedValue;
+
+/**
+ * @Entity
+ * @Table(name="tasks")
+ */
+class Task
+{
+
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $id;
+    private $title;
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
+
